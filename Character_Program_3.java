aae0cadf9499495ce5729b6bc03757ac0d8fcf36
Kiran Sawade <sawadekiran2006@gmail.com>
2026-08-26 16:45:30 +0530

    Add files via upload

---
+++ Character_Program_3.java
@@ -0,0 +1,14 @@
+package javabasics;
+
+public class Character_Program_3 {
+	public static void main(String[] args) {
+		char c = 'd';
+		
+		if(c>='A' && c<='Z') {
+			System.out.println("Uppercase");
+		}	
+		else {
+			System.out.println("Lowercase");
+		}
+	}
+}
