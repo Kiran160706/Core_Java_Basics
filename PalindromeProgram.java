f6b4feeccb3bc1f948f5abf9aa8ef0316c2659ab
Kiran Sawade <sawadekiran2006@gmail.com>
2026-09-01 22:47:16 +0530

    Add files via upload

---
+++ PalindromeProgram.java
@@ -0,0 +1,28 @@
+package javabasics;
+
+import java.util.Scanner;
+
+public class PalindromeProgram {
+	
+	public static void main(String[] args) {
+		// to print a word after line by line and checking whether it is a palindrome...
+		String s = "wow madam wow"; 
+		s=s+" ";
+		String word="";
+		
+		for(int i=0; i<s.length(); i++) {
+			char c=s.charAt(i);
+			
+			if(c != ' ') {
+				word = word+c;
+			}
+			else {
+				
+			}
+			
+		}
+		
+		
+	}
+
+}
